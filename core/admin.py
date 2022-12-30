2158443fbec9d50ef6374db2e28fdc979b7b8fac
jashandeep singh <jashandeep1659@gmail.com>
2022-12-30 09:52:44 +0530

    part04

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Category, Blog
 
-admin.site.register(Category)
-admin.site.register(Blog)
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "writer", "updated", "created")
+    prepopulated_fields = {"slug": ("title",)}
+
+
+class CategoroyAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "updated", "created")
+    prepopulated_fields = {"slug": ("name",)}
+
+
+admin.site.register(Category, CategoroyAdmin)
+admin.site.register(Blog, BlogAdmin)
--- core/models.py
+++ core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from tinymce.models import HTMLField
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
+from django.utils.text import slugify
 
 # Create your models here.
 class Category(models.Model):
@@ -11,6 +12,13 @@ class Category(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Blog, self).save(*args, **kwargs)
+
 
 class Blog(models.Model):
     id = models.AutoField(primary_key=True)
@@ -18,9 +26,16 @@ class Blog(models.Model):
     slug = models.SlugField(max_length=150, unique=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
-    thubmnail = models.ImageField(upload_to="blog/thubmnail")
+    thumbnail = models.ImageField(upload_to="blog/thubmnail")
     breif_info = models.CharField(max_length=200)
     content = HTMLField()
     tags = TaggableManager()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Blog, self).save(*args, **kwargs)
